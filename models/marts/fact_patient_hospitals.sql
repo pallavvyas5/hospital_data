9e41610160f91f3a5c364902a62d097a9ce7f19c
pallavvyas5 <pallavvyas5@gmail.com>
2024-07-09 23:44:22 +0530

    creating fact model

--- models/marts/fact_patient_hospitals.sql
+++ models/marts/fact_patient_hospitals.sql
@@ -6,12 +6,8 @@
 }}
 
 select 
-    staging_p.Patient_ID,
-    staging_d.Doctor_ID,	
-    changes_pd.BILLING_AMOUNT,	
-    changes_pd.ROOM_NUMBER
-from {{ ref('changes_patient_data') }} as changes_pd
-join {{ ref('staging_patients') }} as staging_p
-    on changes_pd.Patient_ID = staging_p.Patient_ID
-join {{ ref('staging_doctors') }} as staging_d
-    on changes_pd.Doctor_ID = staging_d.Doctor_ID
+    Patient_ID,
+    Doctor_ID,	
+    BILLING_AMOUNT,	
+    ROOM_NUMBER
+from {{ ref('changes_patient_data') }} 
